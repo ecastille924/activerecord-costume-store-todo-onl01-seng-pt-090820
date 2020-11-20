2e947ec414064d4bfcf0afedf5c7614e82d48823
Erick Castille <ecastille924@gmail.com>
2020-11-20 01:07:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location 
       t.integer :costume_inventory 
-      t.integer :number_of_employees
-      t.boolean :in_business 
-      t.time :opening_time 
-      t.time :closing_time
+      t.integer :num_of_employees
+      t.boolean :still_in_business 
+      t.datetime :opening_time 
+      t.datetime :closing_time
     end
   end
 end
